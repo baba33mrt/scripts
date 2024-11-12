83d142a2c2a26273fc69e10eda145a87c261ab04
Martin Baptiste <baptiste18032004@gmail.com>
2024-11-12 16:46:33 +0100

    Update install-mongodb (1).sh

--- install-mongodb (1).sh
+++ install-mongodb (1).sh
@@ -2,108 +2,127 @@
 
 #########################################
 # MARTIN Baptiste -- 2024-11-12
-# freeRadius installation on Ubuntu 22.04
-#
-# A lancer depuis user supervisor sans
-# sudo, mot de passe prompt apr\u00e8s
+# FreeRadius installation and configuration for TP on Ubuntu 22.04
+# Run as a user supervisor without sudo, password prompt after
 #########################################
 
-show_time ()
-{
+# Function to show the current time with a message
+show_time () {
     echo -n "$(date +%r)-- "
 }
 
-check_status ()
-{
+# Function to check the status of the last command and exit if it failed
+check_status () {
     if [ $? -eq 0 ]; then
         echo "done"
     else
-        cat /dev/null
-        echo -e "failed\n\nAn error occured\nI made the program quit\n\n"
+        echo -e "failed\n\nAn error occurred\nExiting the program\n\n"
         exit 1
     fi
 }
 
-hidden_check_status ()
-{
-    if [ $? -ne 0 ]; then
-        cat /dev/null
-        echo -e "failed\n\nAn error occured during the installation\nI made the program quit\n\n"
-        exit 1
-    fi
-}
-
-add_proxy()
-{
+# Step 1: Adding proxy configuration
+add_proxy() {
     show_time
     echo -n "Adding proxy..."
-    sudo echo "Acquire {
+    sudo tee /etc/apt/apt.conf.d/proxy > /dev/null <<EOF
+Acquire {
   HTTP::proxy "http://cache.univ-pau.fr:3128";
   HTTPS::proxy "http://cache.univ-pau.fr:3128";
-}" > /etc/apt/apt.conf.d/proxy
+}
+EOF
+    check_status
 }
 
-updating ()
-{
+# Step 2: Update package list and install FreeRADIUS
+update_packages () {
     show_time
     echo -n "Running apt update..."
     sudo apt update &> /dev/null
     check_status
 }
 
-
-install_freeradius ()
-{
+install_freeradius () {
     show_time
     echo -n "Installing FreeRadius..."
     sudo apt install -y freeradius &> /dev/null
     check_status
 }
 
+# Step 3: Configure FreeRADIUS files
+configure_freeradius () {
+    show_time
+    echo -n "Configuring FreeRadius..."
+    
+    # Backup and clean radiusd.conf file
+    sudo cp /etc/freeradius/3.0/radiusd.conf /etc/freeradius/3.0/radiusd.conf.bak
+    sudo grep -v "^\s*#" /etc/freeradius/3.0/radiusd.conf | grep -v "^$" | sudo tee /etc/freeradius/3.0/radiusd.conf.clean > /dev/null
+    
+    # Backup and clean default file
+    sudo cp /etc/freeradius/3.0/sites-available/default /etc/freeradius/3.0/sites-available/default.orig
+    sudo grep -v "^\s*#" /etc/freeradius/3.0/sites-available/default | grep -v "^$" | sudo tee /etc/freeradius/3.0/sites-available/default.clean > /dev/null
+    
+    # Add users to FreeRADIUS users file
+    sudo tee -a /etc/freeradius/3.0/users > /dev/null <<EOF
+anthony Cleartext-Password := "tony"
+    Tunnel-Private-Group-Id = 3
 
+jjb Cleartext-Password := "sesame"
+    Tunnel-Private-Group-Id = 4
+EOF
+    check_status
+}
 
-installation_is_done ()
-{
-    echo -e "\n\nInstallation is done\n\n"
-    echo "Folder is /etc/freeradius/3.0/"
-    installed=("yes")
-	select item in "${installed[@]}" Quit
-	    do
-	        
-                case $REPLY in
-		    1) sudo cd /etc/freeradius/3.0/;;
+# Step 4: Restart FreeRADIUS in debug mode to check configuration
+restart_freeradius_debug () {
+    show_time
+    echo -n "Restarting FreeRadius in debug mode..."
+    sudo /usr/sbin/freeradius -X &
+    check_status
+}
 
-                    $((${#installed[@]}+1))) echo "We're done!"; break;;
-        *) echo "Ooops - unknown choice $REPLY";;
-    esac
-done
+# Step 5: Configure NAS (router)
+configure_nas () {
+    show_time
+    echo -n "Configuring NAS (router)..."
+    # Commands to configure Cisco router as NAS (this is pseudocode, execute on Cisco CLI)
+    echo "Configuring AAA authentication on router"
+    echo "aaa new-model"
+    echo "aaa authentication login default group radius"
+    echo "radius-server host <IP_SERVER> auth-port 1812 acct-port 1813 key <SECRET_KEY>"
+    echo "line vty 0 4"
+    echo "login authentication default"
+    echo "exit"
+    check_status
+}
 
-    echo
+# Function to indicate installation completion
+installation_done () {
+    echo -e "\n\nInstallation and Configuration is complete.\n"
+    echo "FreeRadius is installed and configured at /etc/freeradius/3.0/"
     exit 0
 }
 
-install ()
-{
-    sudo echo
+# Main install function to run all steps
+install () {
     clear
     add_proxy
-    updating
+    update_packages
     install_freeradius
-    installation_is_done
+    configure_freeradius
+    restart_freeradius_debug
+    configure_nas
+    installation_done
 }
 
-
-
-	
-items=("Install" "Clear config" "Item 3")
-select item in "${items[@]}" Quit
+# Main script execution starts here
+items=("Install" "Clear config" "Quit")
+select item in "${items[@]}"
 do
     case $REPLY in
         1) install;;
         2) echo "Selected item #$REPLY which means $item";;
-        3) echo "Selected item #$REPLY which means $item";;
-        
-        $((${#items[@]}+1))) echo "We're done!"; break;;
-        *) echo "Ooops - unknown choice $REPLY";;
+        3) echo "Exiting..."; break;;
+        *) echo "Oops - unknown choice $REPLY";;
     esac
 done
